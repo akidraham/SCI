7c67cf2030ee8863a173cf76b4b5a232727017f7
akidraham <saptadimahardhika@gmail.com>
2025-03-23 14:01:18 +0700

    [add] some backend code

--- public_html/products/products.php
+++ public_html/products/products.php
@@ -1,3 +1,23 @@
+<?php
+// products.php
+
+// Load config and dependencies
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../config/user_actions_config.php';
+
+// start session if not already started
+startSession(); // from user_actions_config.php
+
+// Load dynamic URL configuration based on environment
+$config = getEnvironmentConfig(); // Load environment configuration from config.php
+$baseUrl = getBaseUrl($config, $_ENV['LIVE_URL']); // Get the base URL from the configuration from config.php
+$isLive = $config['is_live'];
+// Detect environment
+$isLiveEnvironment = ($config['BASE_URL'] === $_ENV['LIVE_URL']);
+setCacheHeaders($isLive); // Set header no cache in local from config.php
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
