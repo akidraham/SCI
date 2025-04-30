638ce06d675df61a7540505207982497a5dfd2f5
akidraham <saptadimahardhika@gmail.com>
2025-04-30 17:33:13 +0700

    [add] testimoni.php lupa tidak terupload karena masuk di gitignore

---
+++ public_html/includes/testimoni.php
@@ -0,0 +1,126 @@
+<?php
+// testimoni.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <!--========== Area Testimoni ==========-->
+    <section class="area-testimoni">
+        <div class="heading_2">
+            <h2>Testimoni</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <!-- PLACEHOLDER -->
+            </div>
+            <div class="col-sm-6">
+                <div class="testimoni-inner p-4 mb-4 shadow rounded-4 border-1">
+                    <div class="testimoni-inner-upperarea mb-2">
+                        <img class="testimoni-profilepicture" src="./assets/images/profilepictures/female.svg" alt=""
+                            height="32" width="32" />
+                        <h5 class="testimoni-nama">Klien I******</h5>
+                    </div>
+                    <div class="testimoni-starandtitle mb-2">
+                        <div class="testimoni-stars">
+                            <!-- Bintang ulasan -->
+                            <i class="a-star-5"></i>
+                        </div>
+                        <div class="testimoni-title">
+                            <!-- Judul ulasan -->
+                            <span>"Saya terharu terhadap tim."</span>
+                        </div>
+                    </div>
+                    <div class="review-date mb-2">
+                        <span class="color-secondary size-base">reviewed on Juli 05, 2024</span>
+                    </div>
+                    <div class="produk-dan-varian mb-2">
+                        <span class="color-secondary">Layanan: Skripsi</span>
+                        <i class="text-separator" aria-hidden="true" aria-label="|">&#124;</i>
+                        <span class="color-secondary">Full</span>
+                    </div>
+                    <div class="size-base reviewtext">
+                        <span>Saya benar-benar terharu dengan dedikasi dan profesionalisme! Setiap pekerjaan selalu
+                            diselesaikan dengan tepat waktu dan hasilnya melebihi ekspektasi saya. Kualitas yang
+                            diberikan diluar ekspektasi saya, dan saya sangat menghargai kerja keras serta perhatian
+                            terhadap
+                            detail yang ditunjukkan. Terima kasih, atas pelayanan yang luar biasa!</span>
+                    </div>
+                </div>
+                <div class="testimoni-inner p-4 mb-4 shadow rounded-4 border-1">
+                    <div class="testimoni-inner-upperarea mb-2">
+                        <img class="testimoni-profilepicture" src="./assets/images/profilepictures/female.svg" alt=""
+                            height="32" width="32" />
+                        <h5 class="testimoni-nama">Klien A******</h5>
+                    </div>
+                    <div class="testimoni-starandtitle mb-2">
+                        <div class="testimoni-stars">
+                            <!-- Bintang ulasan -->
+                            <i class="a-star-5"></i>
+                        </div>
+                        <div class="testimoni-title">
+                            <!-- Judul ulasan -->
+                            <span>"Makasih banyak kak! Tinggal nunggu wisuda nih kak."</span>
+                        </div>
+                    </div>
+                    <div class="review-date mb-2">
+                        <span class="color-secondary size-base">reviewed on November 06, 2023</span>
+                    </div>
+                    <div class="produk-dan-varian mb-2">
+                        <span class="color-secondary">Layanan: Skripsi</span>
+                        <i class="text-separator" aria-hidden="true" aria-label="|">&#124;</i>
+                        <span class="color-secondary">Proposal</span>
+                    </div>
+                    <div class="size-base reviewtext">
+                        <span>Aku sudah sidang! Meskipun jawaban aku kadang nggak sesuai sama yang mereka mau, aku tetap
+                            bisa melalui semuanya. Terima kasih atas bantuannya, Kak! Kakak juga kerjanya cepat banget,
+                            sangat membantu! Tinggal nunggu wisuda nih!</span>
+                    </div>
+                </div>
+                <div class="testimoni-inner p-4 mb-4 shadow rounded-4 border-1">
+                    <div class="testimoni-inner-upperarea mb-2">
+                        <img class="testimoni-profilepicture" src="./assets/images/profilepictures/female.svg" alt=""
+                            height="32" width="32" />
+                        <h5 class="testimoni-nama">Klien E******</h5>
+                    </div>
+                    <div class="testimoni-starandtitle mb-2">
+                        <div class="testimoni-stars">
+                            <!-- Bintang ulasan -->
+                            <i class="a-star-5"></i>
+                        </div>
+                        <div class="testimoni-title">
+                            <!-- Judul ulasan -->
+                            <span>"Layanan sangat memuaskan!"</span>
+                        </div>
+                    </div>
+                    <div class="review-date mb-2">
+                        <span class="color-secondary size-base">reviewed on Juli 14, 2022</span>
+                    </div>
+                    <div class="produk-dan-varian mb-2">
+                        <span class="color-secondary">Layanan: Skripsi</span>
+                        <i class="text-separator" aria-hidden="true" aria-label="|">&#124;</i>
+                        <span class="color-secondary">Judul</span>
+                    </div>
+                    <div class="size-base reviewtext">
+                        <span>
+                            Kak, dosen saya menyarankan untuk mengganti objek penelitian, tapi alhamdulillah
+                            sekarang sudah ACC! Rasanya lega sekali. Sekarang saya hanya perlu melanjutkan dengan
+                            menyusun Bab 1. Terima kasih banyak, Kak! &#x1F62D;
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <!-- PLACEHOLDER -->
+        </div>
+    </section>
+    <!--========== Akhir Area Testimoni ==========-->
+</body>
+
+</html>
